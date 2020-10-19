b7bebe277d2b52a76b9f35744a34b67e2e08445e
denoide <denoide1@gmail.com>
2020-10-19 11:53:21 +0200

    initial release

--- Prova.java
+++ Prova.java
@@ -3,6 +3,8 @@ public class Prova{
     public static void main(String[] args){
 
         System.out.println("Snitch & Rat - Pullo fritto");
+        System.out.println("Snitch & Rat - Pullo fritto");
+
 
     }
 
